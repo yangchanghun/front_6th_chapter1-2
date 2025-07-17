a31802cc49f4c23e2adc051ea2400cd576d692ee
yangchanghun <gory4848@naver.com>
2025-07-17 22:50:09 +0900

    createVnode.js구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -12,17 +12,3 @@ export function createVNode(type, props, ...children) {
 
 // children.flat().flat() => 무한 평탄화 => flat(Infinity)
 // 자식 false인것들은 제거 ..,,?
-
-// createVNode(
-//   "div",
-//   null,
-//   true && createVNode("span", null, "Shown"),
-//   false && createVNode("span", null, "Hidden")
-// )
-
-// createVNode(
-//   "div",
-//   null,
-//   createVNode("span", null, "Shown"),
-//   false
-// )
